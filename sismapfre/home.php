552b55c49ac32f91d8de7cc421a6e3dbd3d80def
Ricardo José Lopez <rjlope2@Mapfre.ar>
2019-03-18 14:05:04 -0300

    primer commit

---
+++ sismapfre/home.php
@@ -0,0 +1,16 @@
+<?php require_once 'templates/header.php';?>
+	<div class="content">
+     	<div class="container">
+     		<div class="col-md-8 col-sm-8 col-xs-12">
+     			<div class="well text-center">En hora buena tioooo
+     			</div>
+     			<br>
+     			
+     			
+     		</div>
+     		<?php require_once 'templates/sidebar.php';?>
+     		
+     	</div>
+    </div> <!-- /container -->
+<?php require_once 'templates/footer.php';?>
+	
---
+++ sismapfre/templates/footer.php
@@ -0,0 +1,10 @@
+<div class="clearfix"></div>
+	<!-- Footer -->
+    <footer>
+    	<div class="container-fluid">
+    	</div>
+    </footer>
+    <!-- /Footer -->
+  </body>
+</html>
+<?php ob_end_flush(); ?>
